fda9f6d051d570b0d8bec4fb3620dcf1756e86d7
Bikochii <anton.pelezki@gmail.com>
2023-01-05 15:16:31 +0100

    update

---
+++ bikoflakes/utils/data.js
@@ -0,0 +1,80 @@
+
+const data = {
+    products: [
+        {
+            name: 'BikoFlakes',
+            slug: 'flakes',
+            category: 'desert',
+            image: '/images/Flakes.jpg',
+            price: 420,
+            rating: 4.5,
+            provider: 'Biko',
+            numReviews: 69,
+            description: 'A delicious icecream made with love and care',
+
+        },
+        {
+            name: 'Pizza',
+            slug: 'pizza',
+            category: 'pizza',
+            image: '/images/pizza.jpg',
+            price: 11.99,
+            provider: 'PizzaBlitz',
+            rating: 4.7,
+            numReviews: 66,
+            description: 'A Pizza with your favorite toppings',
+
+        },
+        {
+            name: 'Döner',
+            slug: 'kebab',
+            category: 'kebab',
+            image: '/images/kebab.jpg',
+            price: 4,
+            provider: 'Onkel Grill',
+            rating: 4.9,
+            numReviews: 6612,
+            description: 'The best Kebab in town',
+
+        },
+        {
+            name: 'Soup',
+            slug: 'soup',
+            category: 'soup',
+            image: '/images/soup.jpg',
+            price: 3.99,
+            provider: 'Ganesha',
+            rating: 4.2,
+            numReviews: 62,
+            description: 'Good soup',
+
+        },
+        {
+            name: 'Spaghetti',
+            slug: 'spaghetti',
+            category: 'noodles',
+            image: '/images/spaghetti.jpg',
+            price: 3.99,
+            provider: 'Venecia',
+            rating: 4.2,
+            numReviews: 66,
+            description: 'somebody touched my spaghet',
+
+        }
+        ,
+        {
+            name: 'American Burger',
+            slug: 'burger',
+            category: 'burger',
+            image: '/images/burger.jpg',
+            price: 7.99,
+            provider: 'Bullburger' ,
+            rating: 4.4,
+            numReviews: 1337,
+            description: 'A burger with a lot of delicious meat',
+
+        },
+    ],
+};
+
+export default data;
